aa362a6bf38357a60e29904fb8f780fd0521f5bc
David <davidongaroh@gmail.com>
2022-10-11 13:59:39 +0300

    add user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+    validates :username, uniqueness: true, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :bio, presence: true, length:{minimum:20}
 end
