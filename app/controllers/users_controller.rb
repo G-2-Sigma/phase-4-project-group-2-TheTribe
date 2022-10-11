cfbb55fa29c33e744893d8d3b325d3abfc5df39e
David <davidongaroh@gmail.com>
2022-10-11 14:00:08 +0300

    add user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
 class UsersController < ApplicationController
-    validates :username, uniqueness: true, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :bio, presence: true, length:{minimum:20}
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :posts
     has_many :reviews, through: :posts
     has_many :ratings, through: :posts
+
+    validates :username, uniqueness: true, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :bio, presence: true, length:{minimum:20}
 end
