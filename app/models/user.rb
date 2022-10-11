0c781699c60b3737be05ef5b27432d3d12c05846
David <davidongaroh@gmail.com>
2022-10-11 18:52:18 +0300

    validate email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
     has_many :ratings, through: :posts
 
     validates :username, uniqueness: true, presence: true
-    validates :email, presence: true, uniqueness: true
+    # validates :email, presence: true, uniqueness: true
     validates :bio, presence: true, length:{minimum:20}
+    VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
+    validates :email , presence: true,uniqueness:{case_sensetive:false},
+    format:{with:VALID_EMAIL_REGEX,multiline:true}
 end
