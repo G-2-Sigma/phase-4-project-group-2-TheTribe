ee5f83e4eb6c50b98db267217d62d0a3db7a9414
David <davidongaroh@gmail.com>
2022-10-11 19:17:47 +0300

    change user macro associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
     enum user_type: { regular: 0, author: 1}
     has_secure_password
 
-    has_many :posts
-    has_many :reviews, through: :posts
-    has_many :ratings, through: :posts
+    # has_many :posts
+    # has_many :reviews, through: :posts
+    # has_many :ratings, through: :posts
+    has_many :reviews
+    has_many :posts, through: :reviews
+    has_many :ratings
+    has_many :posts, through: :ratings
 
     validates :username, uniqueness: true, presence: true
     # validates :email, presence: true, uniqueness: true
