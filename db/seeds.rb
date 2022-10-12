5b60674116bc17aa06aa3e1ee819fa392c7fa425
David <davidongaroh@gmail.com>
2022-10-12 22:50:38 +0300

    add posts api data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,37 @@
-# puts "Creating Users"
-# 10.times do
-#     User.create(
-#         username: Faker::Name.name,
-#         email: Faker::Internet.email,
-#         bio: Faker::Lorem.paragraph,
-#         password: Faker::Internet.password(min_length: 8),
-#         profile_picture: Faker::LoremFlickr.image,
-#         user_type: 0
-#     )
-# end
-# puts 'All done Sinyor!'
+puts "Creating Users"
+10.times do
+    User.create(
+        username: Faker::Name.name,
+        email: Faker::Internet.email,
+        bio: Faker::Lorem.paragraph,
+        password: Faker::Internet.password(min_length: 8),
+        profile_picture: Faker::LoremFlickr.image,
+        user_type: 0
+    )
+end
+puts 'All done Sinyor!'
 
-# puts 'Now 🌱  seeding posts...'
+puts 'Now 🌱  seeding posts...'
 
-# response = RestClient.get(
-#     "https://newsapi.org/v2/everything?q=apple&from=2022-10-11&to=2022-10-11&sortBy=popularity&apiKey=a51faed345d34d54961b4577c8755a5f")
-# articles = JSON.parse(response)  
+response = RestClient.get(
+    "https://newsapi.org/v2/everything?q=apple&from=2022-10-11&to=2022-10-11&sortBy=popularity&count=4&apiKey=a51faed345d34d54961b4577c8755a5f")
+posts = JSON.parse(response)  
 
-# categories = ['Travel','Entertaiment','Technology','Fashion','Lifestyle']
+categories = ['Travel','Entertaiment','Technology','Fashion','Lifestyle']
 
-# users = User.all.select{|user|user.user_type =='author'}
+users = User.all.select{|user|user.user_type =='author'}
 
-# articles.each do |post|
-#     new_post = Post.create(
-#         title: post['title'],
-#         content: post['content'],
-#         category: categories.shuffle.first,
-#         user_id: users[rand(0...users.size)].id
-#     )
-# end
-# puts "✅ Done seeding!"
+posts = posts["articles"] 
+
+posts.map do |post|
+    new_post = Post.create(
+        title: post["title"],
+        content: post["content"],
+        category: categories.shuffle.first,
+        user_id: users[rand(0...users.size)].id
+    )
+end
+puts "✅ Done seeding!"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -43,9 +45,9 @@ puts "creating reviews"
         Review.create(
             title: Faker::Name.name,
             comment:Faker::Quote.matz,
+            rating: rand(1..5),
          user_id: User.all[rand(User.count)].id,
     post_id: Post.all[rand(Post.count)].id
         )
     end
     puts "Hey yaaa our reviews done"
-end
