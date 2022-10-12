2e0b0c3cc69772935d754a0645d56ac907f32069
Douglas <douglasobara97@gmail.com>
2022-10-13 01:43:35 +0300

    add navigation bar

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import "./assets/styles/index.css";
 import App from "./layouts/Main/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
