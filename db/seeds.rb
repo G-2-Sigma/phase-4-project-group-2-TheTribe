bf7452c2f35f2071a2ae773782526704b2724289
David <davidongaroh@gmail.com>
2022-10-12 16:21:35 +0300

    add api data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,32 @@
-puts "Creating Users"
-10.times do
-    User.create(
-        username: Faker::Name.name,
-        email: Faker::Internet.email,
-        bio: Faker::Lorem.paragraph,
-        password: Faker::Internet.password(min_length: 8),
-        profile_picture: Faker::LoremFlickr.image,
-        user_type: 0
-    )
-end
-puts 'All done Sinyor!'
+# puts "Creating Users"
+# 10.times do
+#     User.create(
+#         username: Faker::Name.name,
+#         email: Faker::Internet.email,
+#         bio: Faker::Lorem.paragraph,
+#         password: Faker::Internet.password(min_length: 8),
+#         profile_picture: Faker::LoremFlickr.image,
+#         user_type: 0
+#     )
+# end
+# puts 'All done Sinyor!'
 
 # puts 'Now 🌱  seeding posts...'
 
-# response = RestClient.get("
-#     https://newsapi.org/v2/everything?q=apple&from=2022-10-11&to=2022-10-11&sortBy=popularity&apiKey=a51faed345d34d54961b4577c8755a5f"
-# )
-# articles = JSON.parse(response)
-puts "The seeds of posts beginning"
-  10.times do
+# response = RestClient.get(
+#     "https://newsapi.org/v2/everything?q=apple&from=2022-10-11&to=2022-10-11&sortBy=popularity&apiKey=a51faed345d34d54961b4577c8755a5f")
+# articles = JSON.parse(response)  
 
 # categories = ['Travel','Entertaiment','Technology','Fashion','Lifestyle']
+
+# users = User.all.select{|user|user.user_type =='author'}
+
 # articles.each do |post|
-#     post.create(
+#     new_post = Post.create(
 #         title: post['title'],
 #         content: post['content'],
-#         category: categories.shuffle.first
+#         category: categories.shuffle.first,
+#         user_id: users[rand(0...users.size)].id
 #     )
 # end
 # puts "✅ Done seeding!"
