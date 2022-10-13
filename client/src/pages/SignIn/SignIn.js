19157cf8a305a5a7bbc1dd0a873cf4f5be28b633
Douglas <douglasobara97@gmail.com>
2022-10-13 14:46:20 +0300

    complete signIn and SignUp form design

--- client/src/pages/SignIn/SignIn.js
+++ client/src/pages/SignIn/SignIn.js
@@ -1,7 +1,43 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import { BsFillPersonFill } from "react-icons/bs";
+
 
 const SignIn = () => {
-  return <div>SignIn Page</div>;
+  return (
+    <div>
+      <div className="center">
+        <span className="iconRegnLogin">
+          <BsFillPersonFill size={50} />
+        </span>
+        <h1>Login</h1>
+        <form method="post">
+          <div className="txt_field">
+            <Form.Control
+              className="form__input"
+              type="text"
+              placeholder="Enter email"
+            />
+
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div className="txt_field">
+            <Form.Control
+              className="form__input"
+              type="password"
+              placeholder="Password"
+            />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <div className="btn__form">
+            <button className="form__btn">SignIn</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- client/src/pages/SignUp/Signup.js
+++ client/src/pages/SignUp/Signup.js
@@ -1,7 +1,51 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import { BsFillPersonFill } from "react-icons/bs";
 
 const Signup = () => {
-  return <div>Signup page</div>;
+  return (
+    <div>
+      <div className="center">
+        <span className="iconRegnLogin">
+          <BsFillPersonFill size={50} />
+        </span>
+        <h1>Register</h1>
+        <form method="post">
+          <div className="txt_field">
+            <Form.Control
+              className="form__input"
+              type="text"
+              placeholder="Enter email"
+            />
+
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div className="txt_field">
+            <Form.Control
+              className="form__input"
+              type="password"
+              placeholder="Password"
+            />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <div className="txt_field">
+            <Form.Control
+              className="form__input"
+              type="password"
+              placeholder="Confirm Password"
+            />
+            <span></span>
+            <label>Confirm Password</label>
+          </div>
+          <div className="btn__form">
+            <button className="form__btn">SignUp</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
