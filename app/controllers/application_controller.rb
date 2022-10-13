c92b2e72ccb3426357e342cc9dfe2304620e7fd9
brian ambundo <brian.katiechi9@gmail.com>
2022-10-13 12:37:24 +0300

    refined the reviews controller code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,28 @@
 class ApplicationController < ActionController::API
-#   include ActionController::Cookies
+  include ActionController::Cookies
 
-#   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-#   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-#   before_action :authorize
+  before_action :authorize
 
-#   private
+  private
 
-#   def authorize
-#     @user = User.find_by(id: session[:user_id])
+  def authorize
+    @user = User.find_by(id: session[:user_id])
 
-#     render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
-#   end
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
 
-#   def authorize_potential_author
-#     render json:{errors: "You cannot perform that action!"}, status: 401 unless session[:user_type] == "author"
-# end
+  def authorize_potential_author
+    render json:{errors: "You cannot perform that action!"}, status: 401 unless session[:user_type] == "author"
+end
 
-#   def render_unprocessable_entity_response(exception)
-#     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
-#   end
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
-#   def render_not_found_response
-#     render json: { errors: "Item not found" }, status: :not_found
-#   end
+  def render_not_found_response
+    render json: { errors: "Item not found" }, status: :not_found
+  end
 end
