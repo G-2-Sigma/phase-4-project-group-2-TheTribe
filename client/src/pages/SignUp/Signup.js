4dbba2c58a0731b024c91dcee6822ad0bca1c912
Douglas <douglasobara97@gmail.com>
2022-10-13 16:23:57 +0300

    add the signup functionality

--- client/src/pages/SignUp/Signup.js
+++ client/src/pages/SignUp/Signup.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React ,{useState}from "react";
 import Form from "react-bootstrap/Form";
 import { BsFillPersonFill } from "react-icons/bs";
 
-const Signup = () => {
+const Signup = ({setUser}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [bio, setBio] = useState("");
+    const [email, setEmail] = useState("");
+
+
+    function handleSubmit(e) {
+      e.preventDefault();
+      fetch("/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          bio,
+          email,
+          password,
+          password_confirmation: passwordConfirmation,
+
+        }),
+      }).then((r) => {
+        if (r.ok) {
+          r.json().then((user) => setUser(user));
+        }
+      });
+    }
+
   return (
-    <div>
-      <div className="center">
+    <div className="mb-5">
+      <div className="center sigUp">
         <span className="iconRegnLogin">
           <BsFillPersonFill size={50} />
         </span>
         <h1>Register</h1>
-        <form method="post">
+        <form onSubmit={handleSubmit}>
           <div className="txt_field">
             <Form.Control
               className="form__input"
               type="text"
-              placeholder="Enter email"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
 
             <span></span>
@@ -24,8 +55,34 @@ const Signup = () => {
           <div className="txt_field">
             <Form.Control
               className="form__input"
+              type="text"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <span></span>
+            <label>Email</label>
+          </div>
+          <span></span>
+          <label>Bio</label>
+          <div className="txt_field">
+            <Form.Control
+              className="textAreaBio"
+              as="textarea"
+              placeholder="Give you nice bio"
+              style={{ height: "100px" }}
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+            />
+          </div>
+          <div className="txt_field">
+            <Form.Control
+              className="form__input"
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <span></span>
             <label>Password</label>
@@ -35,12 +92,16 @@ const Signup = () => {
               className="form__input"
               type="password"
               placeholder="Confirm Password"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
             />
             <span></span>
             <label>Confirm Password</label>
           </div>
           <div className="btn__form">
-            <button className="form__btn">SignUp</button>
+            <button type="submit" className="form__btn">
+              SignUp
+            </button>
           </div>
         </form>
       </div>
